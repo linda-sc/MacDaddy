616a9e4deddf126a379e2c8dd8210428f78456cd
linda-sc <lindachenny@gmail.com>
2019-07-15 08:46:00 -0400

    7/15/19 - Avatar Object outlined

--- Mac Daddy/AvatarObject.swift
+++ Mac Daddy/AvatarObject.swift
@@ -22,7 +22,7 @@ class AvatarObject: NSObject, Codable {
     var skinTone: String?
     var skinShadow: String?
     var eyeTone: String?
-    var eyebrownTone: String?
+    var eyebrowTone: String?
     
     var hair: String?
     var hairShadow: String?
@@ -96,6 +96,113 @@ class AvatarObject: NSObject, Codable {
         case leftArmShadow
         case rightArm
         case rightArmShadow
+    }
+    
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    // MARK: III. Normal Initialization
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    init(uid: String, email: String) {
+        super.init()
+        self.uid = uid
+    }
+    
+    
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    // MARK: IV. Initialization from decoder
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    
+    //Initializing an object after decoding from JSON.
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: UserCodingKeys.self)
+        
+        uid = try container.decodeIfPresent(String.self, forKey: .uid)
+        colorTone = try container.decodeIfPresent(String.self, forKey: .colorTone)
+        hairTone = try container.decodeIfPresent(String.self, forKey: .hairTone)
+        skinTone = try container.decodeIfPresent(String.self, forKey: .skinTone)
+        skinShadow = try container.decodeIfPresent(String.self, forKey: .skinShadow)
+        eyeTone = try container.decodeIfPresent(String.self, forKey: .eyeTone)
+        eyebrowTone = try container.decodeIfPresent(String.self, forKey: .eyebrowTone)
+
+        hair = try container.decodeIfPresent(String.self, forKey: .hair)
+        hairShadow = try container.decodeIfPresent(String.self, forKey: .hairShadow)
+        hairBack = try container.decodeIfPresent(String.self, forKey: .hairBack)
+        
+        eyes = try container.decodeIfPresent(String.self, forKey: .eyes)
+        irises = try container.decodeIfPresent(String.self, forKey: .irises)
+        eyeBags = try container.decodeIfPresent(String.self, forKey: .eyeBags)
+        eyeSize = try container.decodeIfPresent(Double.self, forKey: .eyeSize)
+        eyeHeight = try container.decodeIfPresent(Double.self, forKey: .eyeHeight)
+        eyeWidth = try container.decodeIfPresent(Double.self, forKey: .eyeWidth)
+
+        nose = try container.decodeIfPresent(String.self, forKey: .nose)
+        noseSize = try container.decodeIfPresent(Double.self, forKey: .noseSize)
+
+        mouth = try container.decodeIfPresent(String.self, forKey: .mouth)
+        mouthShadow = try container.decodeIfPresent(String.self, forKey: .mouthShadow)
+        mouthSize = try container.decodeIfPresent(Double.self, forKey: .mouthSize)
+
+        face = try container.decodeIfPresent(String.self, forKey: .face)
+        faceShadow = try container.decodeIfPresent(String.self, forKey: .faceShadow)
+        neck = try container.decodeIfPresent(String.self, forKey: .neck)
+
+        shirt = try container.decodeIfPresent(String.self, forKey: .shirt)
+        leftArm = try container.decodeIfPresent(String.self, forKey: .leftArm)
+        leftArmShadow = try container.decodeIfPresent(String.self, forKey: .leftArmShadow)
+        rightArm = try container.decodeIfPresent(String.self, forKey: .rightArm)
+        rightArmShadow = try container.decodeIfPresent(String.self, forKey: .rightArmShadow)
+
+    }
+    
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    // MARK: V. Encoding back to JSON.
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    //This is the easy part.
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: UserCodingKeys.self)
+        
+        try container.encodeIfPresent(uid, forKey: .uid)
+        try container.encodeIfPresent(colorTone, forKey: .colorTone)
+        try container.encodeIfPresent(hairTone, forKey: .hairTone)
+        try container.encodeIfPresent(skinTone, forKey: .skinTone)
+        try container.encodeIfPresent(skinShadow, forKey: .skinShadow)
+        try container.encodeIfPresent(eyeTone, forKey: .eyeTone)
+        try container.encodeIfPresent(eyebrowTone, forKey: .eyebrowTone)
+        
+        try container.encodeIfPresent(hair, forKey: .hair)
+        try container.encodeIfPresent(hairShadow, forKey: .hairShadow)
+        try container.encodeIfPresent(hairBack, forKey: .hairBack)
+        
+        try container.encodeIfPresent(eyes, forKey: .eyes)
+        try container.encodeIfPresent(irises, forKey: .irises)
+        try container.encodeIfPresent(eyeBags, forKey: .eyeBags)
+        try container.encodeIfPresent(eyeSize, forKey: .eyeSize)
+        try container.encodeIfPresent(eyeHeight, forKey: .eyeHeight)
+        try container.encodeIfPresent(eyeWidth, forKey: .eyeWidth)
+        
+        try container.encodeIfPresent(nose, forKey: .nose)
+        try container.encodeIfPresent(noseSize, forKey: .noseSize)
+        
+        try container.encodeIfPresent(mouth, forKey: .mouth)
+        try container.encodeIfPresent(mouthShadow, forKey: .mouthShadow)
+        try container.encodeIfPresent(mouthSize, forKey: .mouthSize)
+        
+        try container.encodeIfPresent(face, forKey: .face)
+        try container.encodeIfPresent(faceShadow, forKey: .faceShadow)
+        try container.encodeIfPresent(neck, forKey: .neck)
+        
+        try container.encodeIfPresent(shirt, forKey: .shirt)
+        try container.encodeIfPresent(leftArm, forKey: .leftArm)
+        try container.encodeIfPresent(leftArmShadow, forKey: .leftArmShadow)
+        try container.encodeIfPresent(rightArm, forKey: .rightArm)
+        try container.encodeIfPresent(rightArmShadow, forKey: .rightArmShadow)
     }
     
 }
